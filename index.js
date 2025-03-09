2e1b5dc8503bf1b1b9a794f3c40ca6e7811a1bcf
Elijah <elijah.macharia@student.moringaschool.com>
2025-03-09 21:52:43 +0300

    adjective

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-// code your solution here
+function saturdayFun(activity = "roller-skate") {
+    return `This Saturday, I want to ${activity}!`; // ✅ Fixed to use backticks
+}
+
+function mondayWork(activity = "go to the office") {
+    return `This Monday, I will ${activity}.`; // ✅ Fixed to use backticks and remove extra space
+}
+
+function wrapAdjective(flair = "*") {
+    return function(adjective = "special") {
+        return `You are ${flair}${adjective}${flair}!`; // ✅ Fixed to use backticks
+    };
+}
